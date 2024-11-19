4179400e197c80e5b0bf5427df98ff4d3af66c5f
emporerofSerbia <walidneon231@gmail.com>
2024-11-19 09:30:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+price = 15.00
+print("what you want big boy")
+line = input().split("")
 
+for i in line:
+    if i == "T":
+        price += 1.5
+    elif i == "O":
+        price += 1.25
+    elif i == "P":
+        price += 3.5
+    elif i == "M":
+        price += 3.75
+    elif i == "A":
+        price += 0.4
+
+if price > 20:
+    price *= 0.95
+
+print(f"{price:.2f}")
